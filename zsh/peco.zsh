cffc6de792e93fd1889917f5cc9ada68dfd1d8be
Masaki Takeuchi <m.ishihara@gmail.com>
2014-08-11 07:48:10 +0900

    peco の設定を追加

---
+++ zsh/peco.zsh
@@ -0,0 +1,22 @@
+# http://blog.kenjiskywalker.org/blog/2014/06/12/peco/
+function peco-select-history() {
+    local tac
+    if which tac > /dev/null; then
+        tac="tac"
+    else
+        tac="tail -r"
+    fi
+    BUFFER=$(history -n 1 | \
+        eval $tac | \
+        peco --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
+
+function look() {
+  SHELL=zsh ghq look $(ghq list | peco)
+}
+
+alias -g SRC='~/src/$(ghq list | peco)'
