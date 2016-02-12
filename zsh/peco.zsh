78bad0136ec9669d42ada991ab0aab23e2bb77f2
Masaki Takeuchi <m.ishihara@gmail.com>
2016-02-12 08:56:46 +0000

    zsh: Add global aliases (DPS, DPSA)

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -1,23 +1,17 @@
 # http://blog.kenjiskywalker.org/blog/2014/06/12/peco/
 function peco-select-history() {
-    local tac
-    if which tac > /dev/null; then
-        tac="tac"
-    else
-        tac="tail -r"
-    fi
-    BUFFER=$(history -n 1 | \
-        eval $tac | \
-        peco --query "$LBUFFER")
-    CURSOR=$#BUFFER
+  local tac=tac
+  if ! type tac > /dev/null; then
+    tac='tail -r'
+  fi
+  BUFFER=$(history -n 1 | $tac | peco --query "$LBUFFER")
+  CURSOR=$#BUFFER
 }
-if type peco >/dev/null; then
+if which peco >/dev/null; then
   zle -N peco-select-history
   bindkey '^r' peco-select-history
 fi
 
-function look() {
-  SHELL=zsh ghq look $(ghq list | peco)
-}
-
+alias -g DPS='$(docker ps | tail -n +2 | peco | cut -d " " -f 1)'
+alias -g DPSA='$(docker ps -a | tail -n +2 | peco | cut -d " " -f 1)'
 alias -g SRC='~/src/$(ghq list | peco)'
