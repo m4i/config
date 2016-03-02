49c842930c8119458e2d206e3e6b8f9952f75aea
Masaki Takeuchi <m.ishihara@gmail.com>
2016-03-02 16:09:45 +0900

    Fix peco-select-history

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -4,7 +4,7 @@ function peco-select-history() {
   if ! type tac > /dev/null; then
     tac='tail -r'
   fi
-  BUFFER=$(history -n 1 | $tac | peco --query "$LBUFFER")
+  BUFFER=$(history -n 1 | eval $tac | peco --query "$LBUFFER")
   CURSOR=$#BUFFER
 }
 if which peco >/dev/null; then
