e63208c7b7476639c1f1c599cad9f5c65f46db48
Masaki Takeuchi <m.ishihara@gmail.com>
2016-03-15 15:58:22 +0900

    sh: Refactor _add_path

--- sh/env.sh
+++ sh/env.sh
@@ -2,48 +2,46 @@
 
 ### Functions {{{1
 
-_add_path() {
-  local direction="$1"
-  local name="$2"
-  local directory="$3"
-
-  if [[ ! -d "$directory" ]]; then
-    return
-  fi
-
-  local _path="$(eval echo \$$name)"
-
-  if [[ -z "$_path" ]]; then
-    eval $name=\'$directory\'    #'
-
-  else
-    _path="$(echo "$_path" | sed \
-      -e "s!:$directory:!:!g" \
-      -e "s!:$directory\$!!"  \
-      -e "s!^$directory:!!"   \
-    )"
-    if [[ "$direction" = append ]]; then
-      eval $name=\'$_path:$directory\'    #'
-    else
-      eval $name=\'$directory:$_path\'    #'
-    fi
-  fi
-}
-
-_append_path() {
+function _append_path() {
   _add_path append "$1" "$2"
 }
 
-_prepend_path() {
+function _prepend_path() {
   _add_path prepend "$1" "$2"
 }
 
-_prepend_paths() {
-  local name="$1"
-  shift
-  for directory in "$@"; do
-    _add_path prepend "$name" "$directory"
+function _add_path() {
+  local direction="$1"
+  local env_name="$2"
+  local adding_path="$3"
+
+  local paths="$(eval "echo \$$env_name")"
+
+  if echo "$adding_path" | grep -q \*; then
+    local adding_paths=($(eval "ls -d $adding_path" 2>/dev/null))
+  else
+    local adding_paths=("$adding_path")
+  fi
+
+  for path_ in "${adding_paths[@]}"; do
+    if [[ -z "$paths" ]]; then
+      paths="$path_"
+
+    else
+      paths="$(echo "$paths" | sed \
+        -e "s!:$path_:!:!g" \
+        -e "s!:$path_\$!!"  \
+        -e "s!^$path_:!!"   \
+      )"
+      if [[ "$direction" = append ]]; then
+        paths="$paths:$path_"
+      else
+        paths="$path_:$paths"
+      fi
+    fi
   done
+
+  eval $env_name=\'$paths\'
 }
 
 
@@ -56,8 +54,8 @@ export LC_CTYPE=en_US.UTF-8
 
 _prepend_path PATH /usr/local/sbin
 _prepend_path PATH /usr/local/bin
+_prepend_path PATH ~/bin
 _prepend_path PATH ~/.local/bin
-_prepend_paths PATH ~/.local/*/bin
+_prepend_path PATH '~/.local/*/bin'
 _prepend_path PATH ~/src/github.com/m4i/config/bin
 _prepend_path PATH ~/src/bitbucket.org/m4i/config/bin
-_prepend_path PATH ~/bin
