a41d843c3d0a0d08535cdf31a139fb4319bc8bc8
Masaki Takeuchi <m.ishihara@gmail.com>
2016-05-15 23:31:21 +0000

    setup: refactoring

--- sh/ssh_auth_sock.sh
+++ sh/ssh_auth_sock.sh
@@ -2,6 +2,7 @@ sock_symlink=~/tmp/ssh-auth-sock
 
 if [[ -S "$SSH_AUTH_SOCK" ]]; then
   if [[ "$SSH_AUTH_SOCK" =~ ^/tmp/ssh-.*/agent\.[0-9]*$ ]]; then
+    mkdir -p "$(dirname "$sock_symlink")"
     ln -sf "$SSH_AUTH_SOCK" "$sock_symlink" && \
       export SSH_AUTH_SOCK="$sock_symlink"
   fi
